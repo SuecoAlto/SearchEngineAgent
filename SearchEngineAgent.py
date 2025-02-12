d766275b392b076502e19853dad1bb1e4e6f6e3c
suecoalto <suecoalto@gmail.com>
2025-02-12 14:12:36 +0100

    Tavily Search
    Tavily's Search is a search engine built specifically for AI agents (LLMs), delivering real-time, accurate, and factual results at speed.
    In this step i have added the search engine agent file to the project. This file contains the search enginethat will be used to search for information on the web.

--- SearchEngineAgent.py
+++ SearchEngineAgent.py
@@ -9,14 +9,25 @@
 
 from dotenv import load_dotenv
 import os
+from langchain_community.tools.tavily_search import TavilySearchResults # TavilySearchTool
 
 load_dotenv()
 
-LangSmith_api_key = os.getenv("LANG_SMITH_API_KEY")
-LangSmith_tracing = os.getenv("LANGSMITH_TRACING")
-Tivaly_api_key = os.getenv("TIVALY_API_KEY")
+LangSmith_Api_Key = os.getenv("LANG_SMITH_API_KEY")
+LangSmith_Tracing = os.getenv("LANGSMITH_TRACING")
+Tivaly_Api_Key = os.getenv("TIVALY_API_KEY")
 
 # Initialize LangSmith tracing here
 if LangSmith_tracing == "true":
     print("LangSmith tracing is enabled.")
+
+# TavilySearchTool
+Search = TavilySearchResults(max_results=5)
+UserInput = input("What would you like to search for? ")
+SearchResults = Search.invoke(UserInput)
+print(SearchResults)
+# If we want, we can create other tools. Once we have all the tools we want, 
+# we can put them in a list that we will reference later.
+Tools = [Search]
+
 
